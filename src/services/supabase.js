6a20c49fa4040e0d8af055780e9bfaad05b56348
Cevu01 <vuk.v.stankovic01@gmail.com>
2024-12-15 22:53:58 +0100

    Initial commit

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://rhhodycaonwvqvkfaixj.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoaG9keWNhb253dnF2a2ZhaXhqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM4MjI3OTAsImV4cCI6MjA0OTM5ODc5MH0.--JteKVVrdIPit0wBZnxXmFgbL83yWHFWYvnlWMVJPA";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
