5f8f0b1d3ffced00082b3443017c72efe7b56945
Cevu01 <vuk.v.stankovic01@gmail.com>
2025-01-28 18:53:47 +0100

    Implemented login option for users

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 import { Toaster } from "react-hot-toast";
 
 import Dashboard from "./pages/Dashboard";
@@ -29,6 +30,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
+
       {/* Globalni styling moramo ovako dodati */}
       <GlobalStyles />
       <BrowserRouter>
